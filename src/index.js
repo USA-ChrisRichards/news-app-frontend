54512b3a9fa75ab0936902f46856f431543a623d
github username <github email address>
2019-08-19 17:51:48 -0500

    js comments, readme added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// DOMContentLoaded? defer?
+// display images, scrolling feature
+// comment pop up when image clicked, color change, 'visit article site?' button
+    // new comment/edit comment
+// rating click feature
+
+// global variables
+const url = ""
+const 
